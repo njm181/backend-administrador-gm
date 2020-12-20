193452920150e727d0a9e0366f0f1916df2e372a
Nicolas Molina <nmolina@lnexternos.com.ar>
2020-12-20 19:27:42 -0300

    update

---
+++ src/main/java/controller/EventoController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/eventos")
+public class EventoController {
+
+    @RequestMapping("")
+    public String getListaEventos(Model model){
+        //List<Evento> listaEventos = reunionService.getReuniones();
+        //model.addAttribute("eventos", listaEventos);
+        return "evento-crear";
+    }
+}
